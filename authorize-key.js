f9603e20306e53f24e876033e633c5208e128b54
devran <devran@gmail.com>
2017-06-07 11:55:50 +0300

    initial

---
+++ authorize-key.js
@@ -0,0 +1,4 @@
+/**
+ * Created by devran on 07/06/2017.
+ */
+process.stdout.write('echo command="/etc/scripts/access-check.sh",no-user-rc,no-X11-forwarding,no-agent-forwarding,no-pty ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQQx/XqPb6/em8fLwDCQBnCtMwevOtP4pMe43R2OyNDrUMAPxb9InZTocMDIaVV0Lir2mPTxYawMcOQlDnXT0bgECMXt5K2c2NwgUPK0obSOXZ42bA7LpcOEpUYW8SZmzR29V2R6QrUN/nBk6u6GWtvN9UGY28TzWWSh21d60tKyoyXi00IbWvzC3XaJR64GItfODxCFBv3I0JYMKk61o78N2tWFhCEp2wN7hpCM7EqbKG+nHDAGYTBqNTEWXkSAxqQB+uAH5TRfkifLae+WYF68VOxk0ncVZ4yC643YMPrUYBtoU4mu+WjCMK+np7u4QhaSjo93Z3T/lQSQ/5+uZl devran@devrans-MacBook-Pro.local' + '\n');
