31c180df0a383d069b1ac8b982a41e709e9bb7b6
devran <devran@gmail.com>
2017-06-19 02:16:32 +0300

    changes

--- get-authorized-keys.sh
+++ get-authorized-keys.sh
@@ -1,24 +1,63 @@
 #!/usr/bin/node
 
-fs = require('fs');
+const fs = require('fs'),
+      http = require('http'),
+      querystring = require('querystring');
 
-var args = '';
-var userId = 0;
+#var args = '';
+#var userId = '';
 
-process.argv.forEach((val, index) => {
-    args += val + ' ---> ' + index + '\n';
-});
+#process.argv.forEach((val, index) => {
+#    args += val + ' ---> ' + index + '\n';
+#});
 
 if (process.argv[2] !== '' )
 {
-    userId = 1;
+    var postData = querystring.stringify({
+        'pubkey' : process.argv[2]
+    });
+
+    var options = {
+      host: '52.59.205.23',
+      path: '/pubkey',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': postData.length
+      }
+    };
+
+    callback = function(response) {
+      var str = '';
+
+      //another chunk of data has been recieved, so append it to `str`
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      //the whole response has been recieved, so we just print it out here
+      response.on('end', function () {
+        if (str && str != '') {
+            const user = JSON.parse(str);
+            const userId = user._id,
+                  pubKey = user.pubkey;
+
+            process.stdout.write(`command="/etc/scripts/access-check.sh --userId=${ userId }",no-user-rc,no-X11-forwarding,no-agent-forwarding,no-pty ${ pubKey }` + `\n`);
+        }else{
+            process.exit(-1);
+        }
+      });
+    }
+
+    var req = http.request(options, callback).end();
+
+    req.write
+
+}else{
+    process.exit(-1);
 }
 
-
-fs.writeFile('/home/ubuntu/message.txt', args, (err) => {
-    if (err) throw err;
-console.log('The file has been saved!');
-});
-
-
-process.stdout.write(`command="/etc/scripts/access-check.sh --userId=${ userId }",no-user-rc,no-X11-forwarding,no-agent-forwarding,no-pty ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQQx/XqPb6/em8fLwDCQBnCtMwevOtP4pMe43R2OyNDrUMAPxb9InZTocMDIaVV0Lir2mPTxYawMcOQlDnXT0bgECMXt5K2c2NwgUPK0obSOXZ42bA7LpcOEpUYW8SZmzR29V2R6QrUN/nBk6u6GWtvN9UGY28TzWWSh21d60tKyoyXi00IbWvzC3XaJR64GItfODxCFBv3I0JYMKk61o78N2tWFhCEp2wN7hpCM7EqbKG+nHDAGYTBqNTEWXkSAxqQB+uAH5TRfkifLae+WYF68VOxk0ncVZ4yC643YMPrUYBtoU4mu+WjCMK+np7u4QhaSjo93Z3T/lQSQ/5+uZl devran@devrans-MacBook-Pro.local` + `\n`);
+#fs.writeFile('/home/ubuntu/message.txt', args, (err) => {
+#    if (err) throw err;
+#console.log('The file has been saved!');
+#});
